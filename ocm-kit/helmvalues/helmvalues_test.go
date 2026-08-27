5d9d9987523fb4f49b46eadaaab6d21b82201167
Lukas Grundmann <lukas.grundmann@opendefense.cloud>
2026-08-27 10:01:33 +0200

    fix: recovered unit test, which went missing after merging latest main
    Signed-off-by: Lukas Grundmann <lukas.grundmann@opendefense.cloud>

--- ocm-kit/helmvalues/helmvalues_test.go
+++ ocm-kit/helmvalues/helmvalues_test.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	descriptor "ocm.software/open-component-model/bindings/go/descriptor/runtime"
 	v2 "ocm.software/open-component-model/bindings/go/descriptor/v2"
 	ociaccessv1 "ocm.software/open-component-model/bindings/go/oci/spec/access/v1"
@@ -525,4 +528,14 @@ func TestGetRenderingInput_NativeOCIAccess(t *testing.T) {
 			},
 		}, input.OCIResources)
 	}
+}
+
+// Regression test for https://github.com/open-component-model/community/issues/36
+func TestNewAuthClient_WithHelmCredentialsInConfig(t *testing.T) {
+	configPath := filepath.Join(t.TempDir(), "ocmconfig")
+
+	require.NoError(t, os.WriteFile(configPath, []byte(configWithHelmCredentials), 0600))
+
+	_, err := NewAuthClient(t.Context(), configPath)
+	assert.NoError(t, err)
 }
